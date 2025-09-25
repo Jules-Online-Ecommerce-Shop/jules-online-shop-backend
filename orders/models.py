e852acc3f5dd688edb2c58c294cc4b85f064fb61
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-09-25 22:17:12 +0000

    Add order and orderItem models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from core.models import BaseModel
 
-# Create your models here.
+User = get_user_model()
+
+
+class Order(BaseModel):
+    """
+    Represents an instance of an order
+    """
+
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("shipped", "Shipped"),
+        ("delivered", "Delivered"),
+        ("canceled", "Canceled"),
+    ]
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders"
+    )
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="pending"
+    )
+    total_price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00
+    )
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.email}"
+
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='order_items'
+    )
+    product = models.ForeignKey(
+        "catalog.Product", on_delete=models.PROTECT, related_name="order_items"
+    )
+    quantity = models.PositiveIntegerField()
+    price_snapshot = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
