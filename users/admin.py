8168f41948d01d9829cbe2add7fada9b28fd1799
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-09-26 17:08:29 +0000

    Register User model in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "last_login",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("is_active", "is_staff", "is_superuser", "created_at")
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("-created_at",)
+    readonly_fields = ("last_login", "created_at", "updated_at")
+
+    # Unfold customizations
+    list_display_links = ("id", "email")
+    icon_name = "user"
+
+    fieldsets = (
+        (
+            "Account Info",
+            {
+                "fields": ("email", "username", "password"),
+            },
+        ),
+        (
+            "Personal Info",
+            {
+                "fields": ("first_name", "last_name"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "created_at", "updated_at"),
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
