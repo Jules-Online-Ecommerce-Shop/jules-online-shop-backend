4dcee720d1b3aeb162fdc14b44043216bb220463
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-10-10 22:26:56 +0000

    Refactor order routes to accommodate new views

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from orders.views import OrderListCreateView, OrderDetailView
+from orders.views import OrderListView, OrderDetailView
 
 urlpatterns = [
-    path("", OrderListCreateView.as_view(), name="order-list-create"),
+    path("", OrderListView.as_view(), name="order-list"),
     path("<uuid:pk>/", OrderDetailView.as_view(), name="order-detail"),
 ]
